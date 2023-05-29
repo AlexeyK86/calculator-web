887d185f606859f44e7b81e28c3abd9dd17caba8
Алексей <alesha1986@bk.ru>
2023-05-29 19:25:34 +0300

    CalculatorWeb

--- src/main/java/pro/skype/calculatorweb/controller/CalculatorController.java
+++ src/main/java/pro/skype/calculatorweb/controller/CalculatorController.java
@@ -20,24 +20,26 @@ public class CalculatorController {
     }
 
     @GetMapping("/plus")
-    public String sum(@RequestParam int num1, @RequestParam int num2, int result) {
+    public String sum(@RequestParam int num1, @RequestParam int num2) {
+        int result = calculatorService.sum(num1, num2);
         return buildResponse(num1, num2, result, '+');
     }
     @GetMapping("/minus")
-    public String subtract(@RequestParam int num1, @RequestParam int num2); {
+    public String subtract(@RequestParam int num1,@RequestParam int num2) {
         int result = calculatorService.subtract(num1, num2);
         return buildResponse(num1, num2, result, '-');
     }
+
     @GetMapping("/multiply")
-    public String multiply(@RequestParam int num1, @RequestParam int num2); {
+    public String multiply(@RequestParam int num1, @RequestParam int num2) {
         int result = calculatorService.multiply(num1, num2);
         return buildResponse(num1, num2, result, '*');
 
     }
     @GetMapping("/divide")
-    public String divide(@RequestParam int num1, @RequestParam int num2); {
+    public String divide(@RequestParam int num1, @RequestParam int num2) {
         if (num2 == 0) {
-            return "Второй аргумент 0, на 0 делить нельзя".;
+            return "Второй аргумент 0, на 0 делить нельзя";
         }
         int result = calculatorService.divide(num1, num2);
         return buildResponse(num1, num2, result, '/');
--- src/main/java/pro/skype/calculatorweb/service/CalculatorServiceImpl.java
+++ src/main/java/pro/skype/calculatorweb/service/CalculatorServiceImpl.java
@@ -6,13 +6,10 @@ import org.springframework.stereotype.Service;
 public class CalculatorServiceImpl implements CalculatorService {
     @Override
     public int sum(int num1, int num2) {
-
         return num1 + num2;
     }
-
     @Override
     public int subtract(int num1, int num2) {
-
         return num1 - num2;
     }
     @Override
